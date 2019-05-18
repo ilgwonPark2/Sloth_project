450720186873b2e25a4b087960d478b01fc713fa
ilgwonPark2 <ilgwon@interactor.com>
2019-05-18 17:00:15 +0900

    fix #6 test - ilgwon
    test...fix...

--- sloth-electron-vue/src/renderer/components/Shared/header.vue
+++ sloth-electron-vue/src/renderer/components/Shared/header.vue
@@ -4,7 +4,7 @@
       <b-col cols = "3">
         <a style="color:white; font-weight:500; font-size:2rem;">Sloth &trade; </a>
         <b-img src="../../../../static/image/sloth_small.png" alt="sloth" style=" height:100%; margin-bottom:20px;"></b-img>
-      </b-col>
+      </b-col> 
       <b-col cols = "9">
         <b-link to="/"      class="btn btn-light" style="color: #70AE47; font-weight:700; margin-right:30px;">Home</b-link>
         <b-link to="/"      class="btn btn-light" style="color: #70AE47; font-weight:700; margin-right:30px;">Server</b-link>
