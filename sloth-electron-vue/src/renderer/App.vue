8e0fc55b8039542bc38ee6168745b05204d0e2b4
Ubuntu <ubuntu@ip-172-31-16-46.ap-northeast-2.compute.internal>
2019-05-18 05:35:16 +0000

    test

---
+++ sloth-electron-vue/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sloth-electron-vue'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
