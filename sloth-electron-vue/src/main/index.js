f83066368d38d215715f611b32c1ee29ae8eafb0
ilgwonPark2 <ilgwon@interactor.com>
2019-05-18 17:30:24 +0900

    fix #8 UI change window size and title - ilgwon

--- sloth-electron-vue/src/main/index.js
+++ sloth-electron-vue/src/main/index.js
@@ -18,9 +18,9 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 1200,
     useContentSize: true,
-    width: 1000
+    width: 1400
   })
 
   mainWindow.loadURL(winURL)
