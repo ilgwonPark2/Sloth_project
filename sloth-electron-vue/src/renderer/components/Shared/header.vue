516d24eb5056070b40a67f3e0ebc8056eb033d69
ilgwonPark2 <ilgwon@interactor.com>
2019-06-02 15:22:49 +0900

    fix #7 UI change software Icon
     UI change software Icon

--- sloth-electron-vue/src/renderer/components/Shared/header.vue
+++ sloth-electron-vue/src/renderer/components/Shared/header.vue
@@ -3,8 +3,8 @@
     <b-row style="height:50px; background:#70AE47; padding:5px 0px 0px 50px;">
       <b-col cols = "3">
         <a style="color:white; font-weight:500; font-size:2rem;">Sloth &trade; </a>
-        <b-img src="../../../../static/image/sloth_small.png" alt="sloth" style=" height:100%; margin-bottom:20px;"></b-img>
-      </b-col> 
+        <b-img src="./static/image/sloth_small.png" alt="sloth" style=" height:100%; margin-bottom:20px;"></b-img>
+      </b-col>
       <b-col cols = "9">
         <b-link to="/"      class="btn btn-light" style="color: #70AE47; font-weight:700; margin-right:30px;">Home</b-link>
         <b-link to="/"      class="btn btn-light" style="color: #70AE47; font-weight:700; margin-right:30px;">Server</b-link>
@@ -21,12 +21,6 @@
 <script>
 export default {
   name: 'header',
-  // components: { SystemInformation },
-  // methods: {
-  //   open (link) {
-  //     this.$electron.shell.openExternal(link)
-  //   }
-  // }
 }
 </script>
 
--- sloth-electron-vue/src/renderer/components/Tab/Home.vue
+++ sloth-electron-vue/src/renderer/components/Tab/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="background:#545454; height:88vh;">
-    <b-img src="../../../../static/image/logo_medium.png" alt="sloth" style=" margin-bottom:20px; position:fixed; top:30%; left:25%;"></b-img>
+    <b-img src="./static/image/logo_medium.png" alt="sloth" style=" margin-bottom:20px; position:fixed; top:30%; left:25%;"></b-img>
   </div>
 </template>
 
