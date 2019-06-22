1b5a09c6cc08acbe4822ec40e56899809d472988
ilgwonPark2 <ilgwon@interactor.com>
2019-06-22 23:53:21 +0900

    #18 Server // check server status

--- sloth-electron-vue/src/renderer/components/Tab/Server.vue
+++ sloth-electron-vue/src/renderer/components/Tab/Server.vue
@@ -16,14 +16,14 @@
           </b-button>
         </template>
         <template slot="server_removal" slot-scope="row">
-          <b-button size="md" class="fa fa-trash" @click="item_remove = row.item.server_name;"v-b-modal.node-server-remove-modal pill aria-hidden="true">
+          <b-button size="md" class="fa fa-trash" @click="item_remove = row.item;" v-b-modal.node-server-remove-modal pill aria-hidden="true">
           </b-button>
         </template>
       </b-table>
     </b-row>
   </b-container>
 
-  <b-modal centered id="node-server-create-modal" ref="modal" title="Create new node server" @show="resetModal" @hidden="resetModal" @ok="onSubmit">
+  <b-modal centered id="node-server-create-modal" ref="node-server-create-modal" title="Create new node server" @ok="onSubmit">
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
       <b-form-group id="input-group-1" label="Server Name:" label-for="input-1">
         <!-- <b-form-group id="input-group-1" label="Email address:" label-for="input-1" description="We'll never share your email with anyone else."> -->
@@ -37,7 +37,7 @@
     </b-form>
     <template slot="modal-footer" slot-scope="{ ok, cancel }">
      <!-- Emulate built in modal footer ok and cancel button actions -->
-     <b-button size="sm" variant="success" @click="ok()">
+     <b-button size="sm" variant="success" @click="ok();cancel()">
        Create
      </b-button>
      <b-button size="sm" variant="danger" @click="cancel()">
@@ -46,11 +46,11 @@
    </template>
   </b-modal>
 
-  <b-modal centered id="node-server-remove-modal" ref="modal" title="Remove new node server" @show="resetModal" @hidden="resetModal" @ok="onSubmit">
+  <b-modal centered id="node-server-remove-modal" ref="node-server-remove-modal" title="Remove new node server" @ok="onSubmit">
     Are you sure to remove this server?
     <template slot="modal-footer" slot-scope="{ ok, cancel }">
      <!-- Emulate built in modal footer ok and cancel button actions -->
-     <b-button size="sm" variant="success" @click="ok()">
+     <b-button size="sm" variant="success" @click="ok();cancel()">
        Remove
      </b-button>
      <b-button size="sm" variant="danger" @click="cancel()">
@@ -63,6 +63,7 @@
 
 <script>
 const {ipcRenderer} = require('electron')
+const axios = require('axios');
 
 
 export default {
@@ -71,27 +72,13 @@ export default {
     return {
       // Note 'age' is left out and will not appear in the rendered table
       fields: {
-        server_control: {
-          label: 'Start/Stop'
-        },
-        server_name: {
-          label: 'Server'
-        },
-        server_port: {
-          label: 'Port'
-        },
-        "performance.memory": {
-          label: 'Memory'
-        },
-        "performance.cpu": {
-          label: 'CPU'
-        },
-        "performance.size": {
-          label: 'Size'
-        },
-        server_removal: {
-          label: 'Remove'
-        }
+        server_control: { label: 'Start/Stop' },
+        server_name: { label: 'Server' },
+        server_port: { label: 'Port' },
+        "performance.memory": { label: 'Memory' },
+        "performance.cpu": { label: 'CPU'},
+        "performance.size": { label: 'Size' },
+        server_removal: {label: 'Remove'}
       },
       items: [],
       form: {
@@ -103,7 +90,7 @@ export default {
     }
   },
   mounted: function() {
-    ipcRenderer.on('asynchronous-reply', (event, arg) => {
+    ipcRenderer.on('Error', (event, arg) => {
       alert(JSON.stringify(arg))
     });
 
@@ -114,57 +101,79 @@ export default {
   },
   methods: {
     server_info(file){
-      alert("hey, server_info" + file)
-      this.items = null
+      this.items = {}
       var jsonFile = file;
       var jsonArr = []
 
       for (var i = 0; i < jsonFile.servers.length; i++) {
-        var a = {}
-        a["server_control"] = false
-        a["server_name"] = jsonFile.servers[i].server_name
-        a["server_port"] = jsonFile.servers[i].server_port
-        a["performance"] = {
+        var item = {}
+        item["server_control"] = false
+        item["server_name"] = jsonFile.servers[i].server_name
+        item["server_port"] = jsonFile.servers[i].server_port
+        item["performance"] = {
           memory: '20.33MB',
           cpu: i + '%',
           size: '200MB'
         }
-        jsonArr.push(a);
+        jsonArr.push(item);
       }
-      alert(JSON.stringify(jsonArr))
       this.items = jsonArr
+      for (var i = 0; i < jsonFile.servers.length; i++) {
+        this.server_status_check(jsonFile.servers[i].server_port, i)
+      }
     },
     server_toggle(item) {
       var index = this.find_elem(item);
-      if (this.items[index].server_control) {
-        ipcRenderer.send('server_stop', item.server_name);
-      } else {
-        ipcRenderer.send('server_start', item.server_name);
-      }
+      if (this.items[index].server_control) ipcRenderer.send('server_stop', item.server_name);
+      else ipcRenderer.send('server_start', item.server_name);
+
       this.items[index].server_control = !this.items[index].server_control;
     },
-    server_control(item) {},
     server_create(item) {
-      console.log(item);
       ipcRenderer.send('server_create', item);
       ipcRenderer.send('server_info');
     },
     server_remove(item) {
-      // console.log(this.item_remove);
-      ipcRenderer.send('server_remove', this.item_remove);
+      if(item.server_control === true) this.server_toggle(item);
+      ipcRenderer.send('server_remove', item.server_name);
+      ipcRenderer.send('server_info');
+    },
+    server_ControlAll(status){
+      if(status === "StartAll"){
+        // fr
+      } else {
+
+      }
+    },
+    server_status_check(server_port, idx){
+      // Make a request for a user with a given ID
+      // var index = this.find_elem(item);
+      axios.get('http://localhost:' + server_port)
+        .then(response => {
+          // handle success
+          // console.log("response: " + JSON.stringify(response));
+          // var index = this.find_elem(item);
+          this.items[idx].server_control = true;
+          // this.items[i].server
+        })
+        .catch(error => {
+          // handle error
+          // console.log("error: " + error);
+          this.items[idx].server_control = false;
+        })
+        // setTimeout(function(){
+        //     console.log(result)
+        //   }, 200);
     },
     find_elem(item_find) {
       var result;
       this.items.forEach(function(item, index, arr) {
-        if (JSON.stringify(item_find) === JSON.stringify(item)) result = index;
+        if (JSON.stringify(item_find.server_name) === JSON.stringify(item.server_name)) result = index;
       })
-      console.log(result);
       return result;
     },
     onSubmit(evt) {
       evt.preventDefault();
-      // console.log(evt);
-      // console.log(evt.componentId);
       switch (evt.componentId) {
         case "node-server-create-modal":
           this.server_create(this.form);
@@ -176,7 +185,8 @@ export default {
         default:
           // code block
       }
-      this.show = !this.show;
+      this.form.server_name = ''
+      this.form.server_port = ''
     },
     onReset(evt) {
       evt.preventDefault()
@@ -185,9 +195,9 @@ export default {
       this.form.server_port = ''
       // Trick to reset/clear native browser form validation state
       this.show = false
-      // this.$nextTick(() => {
-      //   this.show = true
-      // })
+      this.$nextTick(() => {
+        this.show = true
+      })
     }
   }
 }
