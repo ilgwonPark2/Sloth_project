70cccc2321255a7a6584aa00f9810cc7bdc5d5d9
ilgwonPark2 <ilgwon@interactor.com>
2019-07-21 23:57:38 +0900

    #41 Front// Design tab design enhancement

--- sloth-electron-vue/src/renderer/components/Tab/Design.vue
+++ sloth-electron-vue/src/renderer/components/Tab/Design.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="" style="height:88vh;">
-    <paginate name="items" :list="items" :per="4" class="text-center">
-      <div>
-        <b-img-lazy class="col-sm-6" v-for="item in paginated('items')"  :src="'https://templated.co/items/thumbnails/'+item + '.jpg'" alt="item" style="margin:30px; width:35%;"></b-img-lazy>
-      </div>
-    </paginate>
-
-    <div @click="scrollToTop()" class="text-center" >
-      <paginate-links for="items" :limit="2"
-      class="pagination pagination-md justify-content-center pagaination-sloth"
-      :show-step-links="true"
-      :step-links="{next: ' »', prev: '« '}"
-      ></paginate-links>
+<div class="" style="height:88vh;">
+  <paginate name="items" :list="items" :per="4" class="text-center" style="margin-top:30px">
+    <div>
+       <b-card-group columns class="card-sloth">
+        <b-card no-body
+        class="overflow-hidden col-md-6"
+        v-for="item in paginated('items')"
+        img-alt="Image"
+        style="max-width: 620px;">
+          <b-row no-gutters>
+            <b-col sm="7">
+              <b-card-img :src="'https://templated.co/items/thumbnails/'+item + '.jpg'" class="rounded-0"></b-card-img>
+            </b-col>
+            <b-col sm="5">
+              <b-card-body :title="item.toUpperCase()">
+                <b-card-text class="card-text-sloth" >
+                  <!-- <b-row align-h="around">
+                    <b-button class="col-sm-5"variant="primary" @click="openDemo(item)">Live Demo</b-button>
+                    <b-button class="col-sm-5"variant="success" @click="openDemo(item)">Apply</b-button>
+                  </b-row> -->
+                  <b-row align-h="around">
+                    <b-button class="col-sm-9" variant="primary" @click="openDemo(item)"> Demo</b-button>
+                  </b-row>
+                  <b-row align-h="around" style="margin-top:20%;">
+                    <b-button class="col-sm-9" variant="success" @click="openDemo(item)">  Apply  </b-button>
+                  </b-row>
+                </b-card-text>
+              </b-card-body>
+            </b-col>
+          </b-row>
+        </b-card>
+    </b-card-group>
     </div>
+  </paginate>
+
+  <div @click="scrollToTop()" class="text-center">
+    <paginate-links for="items" :limit="2" class="pagination pagination-md justify-content-center pagaination-sloth" :show-step-links="true" :step-links="{next: ' »', prev: '« '}" style="margin-bottom:50px;"></paginate-links>
   </div>
-
-
+</div>
 </template>
 
 <script>
@@ -49,15 +71,18 @@ export default {
     }
   },
   mounted: {
-    mounted () {
+    mounted() {
       setTimeout(() => {
         this.shown = true
-    }, 1000)
+      }, 1000)
     }
   },
-  methods:{
+  methods: {
     scrollToTop: function() {
       window.scrollTo(0, 0);
+    },
+    openDemo: function(item){
+      window.open('https://templated.co/'+item)
     }
   }
 }
