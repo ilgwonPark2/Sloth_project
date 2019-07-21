be802f0e917d2cd80075b26b951f930e3c8cd272
jihae <wlgo6020@naver.com>
2019-07-22 00:31:42 +0900

    #39

--- sloth-electron-vue/src/renderer/components/Tab/Boiler.vue
+++ sloth-electron-vue/src/renderer/components/Tab/Boiler.vue
@@ -9,20 +9,27 @@
         </button>
     </div> -->
     <!--   -->
-    <b-list-group style="width:80%; margin-top:50px; margin-left:auto; margin-right:auto;">
-      <b-list-group-item variant="light" v-for="(item, index) in items" v-bind:key="index" v-bind:href="item.link" style="margin-top=10vh;" >
+    <b-list-group style="width:75%; margin-top:50px; margin-left:auto; margin-right:auto;">
+      <b-list-group-item variant="light" v-for="(item, index) in items" v-bind:key="index" style="margin-top=10vh;" >
           <div class="img">
-            <b-img left v-bind:src="item.img" v-bind:alt="item.name" rounded="Rounded image" style="width:9%; height:15%"></b-img>
+            <b-img left v-bind:src="item.img" v-bind:alt="item.name" rounded="Rounded image" style="margin-top=10%; width:9%; height:13%"></b-img>
           </div>
-          <div class="content">
+          <div class="content" @click="Golink(item.name, item.link)">
             <h4 style="width=15px; margin-left:10%;">{{item.name}}</h4>
           </div>
           <p class="mb-1" style="margin-left:10%;">{{item.content}}</p>
           <hr>
           <b-list-group horizontal>
-            <b-badge style="margin-left:10%;" variant="success" pill>Watch: {{item.watch}}</b-badge>
-            <b-badge style="margin-left:5px;" variant="primary" pill>Star: {{item.star}} </b-badge>
-            <b-badge style="margin-left:5px;" variant="success" pill>Fork: {{item.fork}}</b-badge>
+            <!-- Place this tag where you want the button to render. -->
+            <!-- <a class="github-button" href="https://github.com/ntkme/github-buttons/fork" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork ntkme/github-buttons on GitHub">Fork</a> -->
+            <!-- <github-button href="https://github.com/ntkme">Follow @ntkme</github-button> -->
+            <!-- <b-badge style="margin-left:10%;" variant="success" pill>Watch: {{item.watch}}</b-badge>
+            <b-badge style="margin-left:5px;" variant="success" pill>Star: {{item.star}} </b-badge>
+            <b-badge style="margin-left:5px;" variant="success" pill>Fork: {{item.fork}}</b-badge> -->
+            <iframe style="margin-left:10%" v-bind:src="item.fork" frameborder="0" scrolling="0" width="150px" height="30px"></iframe>
+            <iframe style="margin-left:10%" v-bind:src="item.star"  frameborder="0" scrolling="0" width="150px" height="30px"></iframe>
+            <iframe style="margin-left:10%" v-bind:src="item.watch" frameborder="0" scrolling="0" width="150px" height="30px"></iframe>
+
           </b-list-group>
         <!-- <div class="content" style="margin-top=10%;">
           
@@ -39,58 +46,49 @@ export default {
     return {
       items: [
         {
-          name: 'hielo',
-          watch: '836',
-          star: '1234',
-          fork: '5029',
-          img: "https://avatars1.githubusercontent.com/u/544954?v=4",
-          content: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.', 
-          link: 'https://templated.co/hielo'
+          name: 'sahat',
+          content: 'A boilerplate for Node.js web applications', 
+          watch: 'https://ghbtns.com/github-btn.html?user=sahat&repo=hackathon-starter&type=watch&count=true&v=2',
+          star: 'https://ghbtns.com/github-btn.html?user=sahat&repo=hackathon-starter&type=star&count=true',
+          fork: 'https://ghbtns.com/github-btn.html?user=sahat&repo=hackathon-starter&type=fork&count=true',
+          img: 'https://avatars0.githubusercontent.com/u/544954?s=400&v=4',
+          link: 'https://github.com/sahat/hackathon-starter'
           },
         {
-          name: 'industrious',          
-          watch: '836',
-          star: '1234',
-          fork: '5029',
-          content: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.', 
-          link: 'https://templated.co/industrious',
-          img: "https://avatars1.githubusercontent.com/u/544954?v=4"
+          name: 'alexa',          
+          content: 'The Alexa Skills Kit SDK for Node.js helps you get a skill up and running quickly, letting you focus on skill logic instead of boilerplate code.', 
+          watch: 'https://ghbtns.com/github-btn.html?user=alexa&repo=alexa-skills-kit-sdk-for-nodejs&type=watch&count=true&v=2',
+          star: 'https://ghbtns.com/github-btn.html?user=alexa&repo=alexa-skills-kit-sdk-for-nodejs&type=star&count=true',
+          fork: 'https://ghbtns.com/github-btn.html?user=alexa&repo=alexa-skills-kit-sdk-for-nodejs&type=fork&count=true',
+          img: "https://avatars3.githubusercontent.com/u/27459759?s=400&v=4",
+          link: 'https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs'
           },
         {
-          name: 'snapshot',
-          watch: '836',
-          star: '1234',
-          fork: '5029',
-          content: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',          
-          link: 'https://templated.co/snapshot',
-          img: "https://avatars1.githubusercontent.com/u/544954?v=4"
+          name: 'kriasoft',
+          watch: 'https://ghbtns.com/github-btn.html?user=kriasoft&repo=nodejs-api-starter&type=watch&count=true&v=2',
+          star: 'https://ghbtns.com/github-btn.html?user=kriasoft&repo=nodejs-api-starter&type=star&count=true',
+          fork: 'https://ghbtns.com/github-btn.html?user=kriasoft&repo=nodejs-api-starter&type=fork&count=true',
+          content: 'Boilerplate and tooling for authoring data API backends with Node.js and GraphQL',          
+          link: 'https://github.com/kriasoft/nodejs-api-starter',
+          img: "https://kriasoft.githubusercontent.com/u/544954?v=4"
           },
         {
-          name: 'roadtrip',          
-          watch: '836',
-          star: '1234',
-          fork: '5029',
-          content: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',  
-          link: 'https://templated.co/roadtrip',
-          img: "https://avatars1.githubusercontent.com/u/544954?v=4"
+          name: 'icebob',          
+          watch: 'https://ghbtns.com/github-btn.html?user=icebob&repo=vue-express-mongo-boilerplate&type=watch&count=true&v=2',
+          star: 'https://ghbtns.com/github-btn.html?user=icebob&repo=vue-express-mongo-boilerplate&type=star&count=true',
+          fork: 'https://ghbtns.com/github-btn.html?user=icebob&repo=vue-express-mongo-boilerplate&type=fork&count=true',
+          content: ':star: MEVN Full stack JS web app boilerplate with NodeJS, Express, Mongo and VueJS',  
+          link: 'https://github.com/icebob/vue-express-mongo-boilerplate',
+          img: "https://avatars0.githubusercontent.com/u/306521?s=400&v=4"
         },
         {
-          name: 'urban',
-          watch: '836',
-          star: '1234',
-          fork: '5029', 
-          content: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',
-          img: "https://avatars1.githubusercontent.com/u/544954?v=4", 
-          link: 'https://templated.co/urban'
-        },
-        {
-          name: 'broadcast',
-          watch: '836',
-          star: '1234',
-          fork: '5029',
-          content: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',  
-          img: "https://avatars1.githubusercontent.com/u/544954?v=4",
-          link: 'https://templated.co/broadcast'
+          name: 'FredericHeem',
+          watch: 'https://ghbtns.com/github-btn.html?user=FredericHeem&repo=starhackit&type=watch&count=true&v=2',
+          star: 'https://ghbtns.com/github-btn.html?user=FredericHeem&repo=starhackit&type=star&count=true',
+          fork: 'https://ghbtns.com/github-btn.html?user=FredericHeem&repo=starhackit&type=fork&count=true', 
+          content: 'StarHackIt: Preact/React Native/Node fullstack starter kit with authentication and authorisation, data backed by SQL.',
+          img: 'https://avatars2.githubusercontent.com/u/4118089?s=400&v=4', 
+          link: 'https://github.com/FredericHeem/starhackit'
         }
       ],
       sort: [
@@ -112,9 +110,8 @@ export default {
     scrollToTop: function() {
       window.scrollTo(0, 0);
     },
-    Golink: function(){
-
-
+    Golink: function(name,link){
+      window.open(link, name, "width=1000, height=800, toolbar=yes, menubar=yes, scrollbars=yes, resizable=yes" )
     }
   }
 }
