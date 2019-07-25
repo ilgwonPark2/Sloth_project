323b0b32bfe6c2e137934a6f0347c6fe6c28318c
ilgwonPark <ilgwon@ds.seoultech.ac.kr>
2019-07-25 15:09:12 +0900

    #52 Server// change timer interval
    experiment..

--- sloth-electron-vue/src/renderer/components/Tab/Server.vue
+++ sloth-electron-vue/src/renderer/components/Tab/Server.vue
@@ -84,13 +84,13 @@ export default {
     }
   },
   mounted: function() {
-    this.server_refresh()
+    // this.server_refresh()
     this.timer = this.server_refresh()
     ipcRenderer.on('Error', (event, arg) => { alert(JSON.stringify(arg)) });
     ipcRenderer.on('server_info_reply', (event, arg) => { this.server_info(arg) });
 
     // refresh timer
-    setInterval(this.server_refresh, 15000);
+    setInterval(this.server_refresh, 25000);
   },
   beforeDestroy() {
     clearInterval(this.timer)
