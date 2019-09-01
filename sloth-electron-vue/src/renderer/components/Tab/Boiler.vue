438fd97ea2ccbca7708a2029b2bfaf0f50845018
jihae <wlgo6020@naver.com>
2019-09-01 16:59:05 +0900

    #69 Boilertab spinner & API

--- sloth-electron-vue/src/renderer/components/Tab/Boiler.vue
+++ sloth-electron-vue/src/renderer/components/Tab/Boiler.vue
@@ -1,25 +1,28 @@
 <template>
-    <b-row align-h="end" style="margin-top:15px;" >
+  <div style="height:86vh;" v-if="status_apply">
+    <div style=" margin-top:20px; margin-left: 1100px">
+      <a style="font-weight:700; color:#424242; font-size:0.8em;">project name </a>
       <button class="fa fa-sort-alpha-desc"  aria-hidden="true" @click="strDesc()"></button>
+      <a style="font-weight:700; color:#424242; font-size:0.8em;">num of stars </a>
       <button class="fa fa-sort-numeric-desc" style="margin-right:50px;" aria-hidden="true" @click="intDesc()"></button>
-  <div style="height:88vh;">
+    </div>
   <paginate name="items" :list="items" :per="4" class="text-center" style="margin-top:30px">
     <div>
       <b-card-group columns class="card-sloth">
         <b-card no-body v-for="item in paginated('items')"  style="margin-top: 20px; width: 650px; height: 280px;" v-bind:key="item">
           <b-row >
-            <b-col sm="3" style="margin-top:15px; margin-left:15px;">
+            <b-col cols="2" style="margin-top:30px; margin-left:30px;">
               <img class="card-img-top" center v-bind:src="item.img" v-bind:alt="item.user"  >   
             </b-col>
-            <b-col sm ="6">
+            <b-col cols ="7">
               <div class="card-body">
                 <h5 class="card-title">{{item.repo}}</h5>
                 <p class="card-text">{{item.content}}</p>
               </div>
              </b-col>
-            <b-col sm="1" style="margin-top:20px;">
-              <iframe v-bind:src="item.forks_count" frameborder="0" scrolling="0" width="150px" height="30px"></iframe>
+            <b-col cols="1" style="margin-top:20px;">
               <iframe v-bind:src="item.stargazers_count"  frameborder="0" scrolling="0" width="150px" height="30px"></iframe>
+              <iframe v-bind:src="item.forks_count" frameborder="0" scrolling="0" width="150px" height="30px"></iframe>
               <iframe v-bind:src="item.watchers_count" frameborder="0" scrolling="0" width="150px" height="30px"></iframe>
             </b-col>
           </b-row>
@@ -33,13 +36,18 @@
     <div @click="scrollToTop()" class="text-center">
       <paginate-links for="items" :limit="3" class="pagination pagination-md justify-content-center pagaination-sloth" :show-step-links="true" :step-links="{next: ' »', prev: '« '}" style="margin-bottom:50px;"></paginate-links>
     </div>
-
   </div>
-</b-row>
+  <div style="height:72vh;" v-else >
+    <h5 v-if="!status_apply" class="text-center"
+    style="margin-top: 150px; margin-left:40px; font-weight:700; color:#424242; font-size:1.6em;" > Loading GitHub information ... </h5>
+    <div class="d-flex justify-content-center" style="width:100%">
+      <div class="spinner-border text-success" role="status" style="width: 3rem; height: 3rem; margin-top: 10px" variant="success" key="success" type="grow"></div>
+    </div>
+  </div>
+
 </template>
 
 <script>
-// import GitHub from 'github-api';
 export default {
   data() {
     return {
@@ -49,51 +57,56 @@ export default {
         {'class':'sort-star-down','title':'sort by stars', selected:true},
         {'class':'sort-alpha-down','title':'sort by alphabet', selected:false}
        
-      ]
-    }
-
+      ],
+      status_apply: false    
+      }
   },
   mounted: function(){
-      this.boiler_list_read();
-      console.log('boiler_ino')
-
+      this.github_info();
+      // console.log('boiler_ino')
   },
   methods:{
     scrollToTop: function() {
       window.scrollTo(0, 0);
     },
-    boiler_list_read(file){
+    github_info: function(){
       var tempArr=[]
-      var Octokit = require('@octokit/rest')
-      var octokit = new Octokit({
-        auth: {
-            username: 'jihaeK',
-            password: 'rlawlgo12',
+      const request = require("request");
+      const self = this
+      const options = {
+        // url:'https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc',
+        url: 'https://api.github.com/search/repositories?q=nodejs&boilerplates+stars:>=1000+language:javascript&sort=stars&order=desc',
+        headers: {
+        "Autorization": "token b12d59da18ca9f268312e489599d178f8412e4b0",
+        'Accept': 'application/vnd.github.mercy-preview+json',
+        "User-Agent": "abc"
         }
-        
-      })
-      octokit.search.repos({
-        q: 'nodejs&boilerplates+stars:>=10+forks:>=12+language:javascript&sort=stars&order=desc',
-        }).then(({ data }) => {
-          console.log(data)
-          for(var i=0; i<data.items.length; i++) {
+      }
+      request.get(options, function (error, response, body) {
+        // var result = new Object(body) 
+        var obj = JSON.parse(body)
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+
+        for(var i=0; i<obj.items.length; i++) {
             var temp={}
-            var count='https://ghbtns.com/github-btn.html?user='+data.items[i].owner.login+'&repo='+data.items[i].name+'&type='
-            temp["user"]=data.items[i].owner.login,
-            temp["repo"]=data.items[i].name,
-            temp["star_value"]=data.items[i].stargazers_count,
+            var count='https://ghbtns.com/github-btn.html?user='+obj.items[i].owner.login+'&repo='+obj.items[i].name+'&type='
+            temp["user"]=obj.items[i].owner.login,
+            temp["repo"]=obj.items[i].name,
+            temp["star_value"]=obj.items[i].stargazers_count,
             temp["watchers_count"]=count+'watch&count=true&v=2',
             temp["stargazers_count"]=count+'star&count=true',
             temp["forks_count"]=count+'fork&count=true',
-            temp["img"]=data.items[i].owner.avatar_url,
-            temp["link"]=data.items[i].html_url,
-            temp["content"]=data.items[i].description
+            temp["img"]=obj.items[i].owner.avatar_url,
+            temp["link"]=obj.items[i].html_url,
+            temp["content"]=obj.items[i].description
             tempArr.push(temp);
           }
-          this.items=tempArr;
-      })
-      console.log("boiler_list_read: ",tempArr)
-      // return tempArr
+        self.items=tempArr;
+        console.log(tempArr)
+        self.status_apply=true    
+      });
     },
     Golink: function(name,link){
       window.open(link, name, "width=1000, height=800, toolbar=yes, menubar=yes, scrollbars=yes, resizable=yes" )
